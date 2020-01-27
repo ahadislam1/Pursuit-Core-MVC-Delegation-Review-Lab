c0f41c1880b3bb107bf99684142aa364f15bf3c5
Ahad Islam <ahad.islam624@gmail.com>
2020-01-27 16:55:53 -0500

    created view controller added extension to conform datasource and delegation.

--- MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/ViewController.swift
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let movies = Movie.allMovies
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
 
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Movie Cell", for: indexPath)
+        cell.textLabel?.text = movies[indexPath.row].name
+        cell.detailTextLabel?.text = "\(movies[indexPath.row].year)"
+        return cell
+    }
+    
+    
 }
 
